1f6c9edeb4aac673b7469cd1cd9ee9d2bfe27789
ivan-kryukov <ivan.o.kryukov@gmail.com>
2015-05-25 10:10:19 -0600

    regex for bedrooms

--- index.js
+++ index.js
@@ -2,31 +2,28 @@ var express = require("express"),
 async = require("async"),
 request = require("request"),
 cheerio = require("cheerio"),
+config = require("./config"),
 app = express();
 
 app.get("/scrape", function (localRequest, localResponse) {
-  var baseURL = "http://vancouver.craigslist.ca";
-
-  var searchURL = baseURL;
-
-  var results = [];
-
-  request(baseURL + "/search/apa", function (err, res, html) {
+  request(config.baseURL + "/search/apa", function (err, res, html) {
     if (!err) {
       var $ = cheerio.load(html);
       var items = $("a[data-id]").map(function (i,e) {
         return $(e).attr("href");
       }).get();
-      // console.log(items);
       async.map(items, function (itemURL, callback) {
-        request(baseURL + itemURL, function (err, res, html) {
+        request(config.baseURL + itemURL, function (err, res, html) {
           if (err) {
             return callback(err);
           } else {
             var $ = cheerio.load(html);
             console.log("Processing " + itemURL);
+            var description = $(".housing").text();
             callback(null, {
               price: $(".price").text(),
+              bedrooms: /(\d+)br/g.exec(description)[1],
+              area: /(\d+)ft2/g.exec(description)[1],
               latitude: $("#map").attr("data-latitude"),
               longitude: $("#map").attr("data-longitude")
             });
@@ -36,28 +33,14 @@ app.get("/scrape", function (localRequest, localResponse) {
         if (err) {
           console.log(err);
         } else {
-          console.log("Success!");
+          console.log("Processed " + results.length + " postings");
           localResponse.send(results);
         }
       });
-      // $("a[data-id]").filter(function () {
-      //   var itemURL = baseURL + $(this).attr("href");
-      //   request(itemURL, function (err, res, html) {
-      //     var $ = cheerio.load(html);
-      //     results.push({
-      //       price: $(".price").text(),
-      //       latitude: $("#map[data-latitude]").text(),
-      //       longitude: $("#map[data-longitude]").text()
-      //     });
-      //   });
-      // });
-
-      // localResponse.send(results);
-
     } else {
       console.log("Error: " + err);
     }
   });
 });
 
-app.listen(8081);
+app.listen(config.port);
